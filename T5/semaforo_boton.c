7d4b60e62978005b10da2eef681a6d5f408a47ef
Camilo Gutierrez <cdgutierrez2@uc.cl>
2022-09-27 18:56:31 -0300

    codigo boton

--- T5/semaforo_boton.c
+++ T5/semaforo_boton.c
@@ -9,16 +9,22 @@
 #define INTC_DEVICE_ID 		XPAR_PS7_SCUGIC_0_DEVICE_ID
 #define TMR_DEVICE_ID		XPAR_TMRCTR_0_DEVICE_ID
 #define LEDS_DEVICE_ID		XPAR_AXI_GPIO_LED_DEVICE_ID
+#define BTNS_DEVICE_ID		XPAR_AXI_GPIO_BUTTONS_DEVICE_ID
 #define INTC_TMR_INTERRUPT_ID XPAR_FABRIC_AXI_TIMER_0_INTERRUPT_INTR
+#define INTC_GPIO_INTERRUPT_ID XPAR_FABRIC_AXI_GPIO_BUTTONS_IP2INTC_IRPT_INTR
 
+#define BTN_INT 			XGPIO_IR_CH1_MASK
 #define TMR_LOAD			(0xFFFFFFFF - 50000000*0.5) // 0.5s Timer
 
-XGpio LEDInst;
+XGpio LEDInst, BTNInst;
 XScuGic INTCInst;
 XTmrCtr TMRInst;
 static int led_data;
+static int btn_value;
+static int boton_press;
 static int tmr_count;
 static int led_out;
+static int max_counter = 15;
 
 //----------------------------------------------------
 // PROTOTYPE FUNCTIONS
@@ -36,9 +42,21 @@ static int IntcInitFunction(u16 DeviceId, XTmrCtr *TmrInstancePtr);
 
 void TMR_Intr_Handler(void *data)
 {
+	// Disable GPIO interrupts
+	XGpio_InterruptDisable(&BTNInst, BTN_INT);
+	// Ignore additional button presses
+	if ((XGpio_InterruptGetStatus(&BTNInst) & BTN_INT) !=
+			BTN_INT) {
+			return;
+		}
+
 	if (XTmrCtr_IsExpired(&TMRInst,0)){
 
 		XTmrCtr_Reset(&TMRInst,0);
+
+		btn_value = XGpio_DiscreteRead(&BTNInst, 1);
+
+		boton_press = btn_value;
 
 		if(tmr_count == 1){ // La segunda vez que entre (2 veces 0.5 segundos = 1 segundo)
 							// se suma uno a la data del led
@@ -53,15 +71,25 @@ void TMR_Intr_Handler(void *data)
 			}
 			else if(led_data >= 7){
 				led_out = 9;
+				// si se presiona el boton aumenta max_counter
+				if (boton_press == 1 && led_counter == 7){
+          max_counter = 17;
+          boton_press = 0;
+        }
 			}
 			led_data = led_data + 1;
-			if (led_data == 15){
+			if (led_data == max_counter){
 				led_data = 0;
+				max_counter = 15;
 			}
 			XGpio_DiscreteWrite(&LEDInst, 1, led_out);
 		}
 		else tmr_count++;
 	}
+
+	(void)XGpio_InterruptClear(&BTNInst, BTN_INT);
+  // Enable GPIO interrupts
+  XGpio_InterruptEnable(&BTNInst, BTN_INT);
 }
 
 
